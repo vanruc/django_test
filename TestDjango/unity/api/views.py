e567c2aaaea8de0df8c9c5d82564764989e96367
vanruc <rucvan.pr@gmail.com>
2022-09-29 14:16:04 +0700

    validate email address

--- TestDjango/unity/api/views.py
+++ TestDjango/unity/api/views.py
@@ -1,8 +1,10 @@
+import django.core.exceptions
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from unity.models import Leads
 from unity.api.serializers import LeadsSerializer
+from django.core.validators import validate_email
 
 
 class LeadsListView(generics.ListAPIView):
@@ -12,8 +14,19 @@ class LeadsListView(generics.ListAPIView):
 
 class LeadsSubscribe(APIView):
     def post(self, request):
-        leads = Leads(email_id=request.data['email'])
-        leads.save()
-        return Response({
-            'subscribed': True
-        })
+
+        ressult_str = {}
+        req_email = request.data['email']
+        try:
+            validate_email(req_email)
+            leads = Leads(email_id=request.data['email'])
+            leads.save()
+            ressult_str = {
+                'subscribed': 'success'
+            }
+        except django.core.exceptions.ValidationError as e:
+            ressult_str = {
+                'issue': e.message
+            }
+
+        return Response(ressult_str)
